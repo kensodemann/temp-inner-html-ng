11f7df46132874e64e8be42e39a8f318bcbee001
Ken Sodemann <ken@ionic.io>
2024-08-02 09:43:41 -0500

    implement the innter HTML stuff

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  endingHtml = '<h1>Done...</h1><p>And now my brain is fried. Oooops...</p>';
+  startingHtml = '<h1>Thinking...</h1><p>This is taking my brain a while. Hang on...</p>';
+  theHtml = '<h1>HTML</h1><p>This is some strange HTML</p>';
+  theState = 0;
 
-  constructor() {}
-
+  constructor(public sanitizer: DomSanitizer) {
+    setTimeout(() => {
+      this.theState = 1;
+    }, 5000);
+  }
 }
